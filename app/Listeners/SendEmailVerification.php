94ac250a99b90a6dc2798dd0c8850ade851b8eb5
ZuhairMahjoub <zouhairmh43@gmail.com>
2026-05-22 20:08:56 +0300

    Add logging to email verification listener

--- app/Listeners/SendEmailVerification.php
+++ app/Listeners/SendEmailVerification.php
@@ -5,12 +5,15 @@ namespace App\Listeners;
 use App\Events\UserRegistered;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
 
 class SendEmailVerification implements ShouldQueue
 {
     /**
      * Create the event listener.
      */
+    public $tries = 3;      // حاول 3 مرات قبل أن تعطي FAIL
+    public $backoff = 10;
     public function __construct()
     {
         //
@@ -21,6 +24,8 @@ class SendEmailVerification implements ShouldQueue
      */
  public function handle(UserRegistered $event): void
 {
+    Log::info('--- بدأت عملية إرسال الإيميل للمستخدم: ' . $event->user->email);
+    
     if (!$event->user->email) {
         return;
     }
