9693c90e2dc89105f9a0769acf3aabc13b1760d5
ZuhairMahjoub <zouhairmh43@gmail.com>
2026-06-20 13:09:43 +0300

    Last version of the project

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -18,7 +18,7 @@ class BookingController extends Controller
 
     public function store(StoreBookingRequest $request): JsonResponse
     {
-        $data = BookingData::fromRequest(
+        $data =  \App\Models\Booking::fromRequest(
             $request->validated(),
             $request->user()->id
         );
