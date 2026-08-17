6fefba52db3ed03683cb6b47ed90947df8541022
ZuhairMahjoub <zouhairmh43@gmail.com>
2026-08-17 03:02:56 +0300

    fix(seeder): bump remaining_capacity to 1000 for hall/service/package slots

--- app/Models/ListingSlot.php
+++ app/Models/ListingSlot.php
@@ -11,7 +11,7 @@ use Spatie\Translatable\HasTranslations;
 
 class ListingSlot extends Model
 {
-    use HasFactory, HasUlids;
+    
 
     use HasFactory, HasUlids,HasTranslations;
     public $translatable = ['slot_name'];
